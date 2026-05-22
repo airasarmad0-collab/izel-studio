4be7de9ac4f86e2176dd336cfb34f9de05dee752
Sheharyar Sarmad <airasarmad0@gmail.com>
2026-05-22 12:06:20 +0500

    fix: VITE env variable for production

--- client/src/common/api.jsx
+++ client/src/common/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiBase = axios.create({
-  baseURL: import.meta.env.VITE_API_LINK,
+  baseURL: import.meta.env.API_LINK,
   headers: { "Content-Type": "application/json" },
 });
 
