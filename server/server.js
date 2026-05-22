88b0a01d9cf6ce4fc9d6407834b749e90a7b3c71
Sheharyar Sarmad <airasarmad0@gmail.com>
2026-05-22 08:53:54 +0500

    fix render mongodb connection and server startup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 const http = require("http");
 const app = require("./src/app");
 const dbConnection = require("./src/config/db.connection");
-const { port } = require("./src/config/env.secrets");
 const express = require("express");
 const path = require("path")
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
+const port = process.env.PORT;
 const server = http.createServer(app);
 
 const startServer = async () => {
--- server/src/config/db.connection.js
+++ server/src/config/db.connection.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
-const { mongoUri } = require("./env.secrets");
 
 const dbConnection = async () => {
-    try {
-        await mongoose.connect(mongoUri);
-        console.log("DB connected successfully");
-    } catch (err) {
-        console.error("DB connection failed:", err.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB connected successfully");
+  } catch (err) {
+    console.error("DB connection failed:", err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = dbConnection;
