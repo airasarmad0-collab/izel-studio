8383c49167595d58029482aca6d59a89f5a46510
Sheharyar Sarmad <airasarmad0@gmail.com>
2026-05-23 15:58:54 +0500

    fix volume delete API and auth issue

--- client/src/common/api.jsx
+++ client/src/common/api.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 const apiBase = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: { "Content-Type": "application/json" },
+  withCredentials: true
 });
 
 console.log(import.meta.env.VITE_API_URL);
