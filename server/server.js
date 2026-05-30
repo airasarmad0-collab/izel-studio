d365275349ccc46749ab89026d05b961b4b1bfff
Sheharyar Sarmad <airasarmad0@gmail.com>
2026-05-30 20:01:05 +0500

    a

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const dbConnection = require("./src/config/db.connection");
 const express = require("express");
 const path = require("path")
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 
